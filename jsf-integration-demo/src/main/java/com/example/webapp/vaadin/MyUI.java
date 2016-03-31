312c93890ac9bde50c9036584b9f21db1b6c7b8f
Alejandro Duarte <alejandro@vaadin.com>
2016-03-31 13:34:05 +0300

    Added custom theme to demo

--- jsf-integration-demo/src/main/java/com/example/webapp/vaadin/MyUI.java
+++ jsf-integration-demo/src/main/java/com/example/webapp/vaadin/MyUI.java
@@ -1,15 +1,39 @@
 package com.example.webapp.vaadin;
 
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 
-public class MyUI extends UI {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyUI extends UI implements Button.ClickListener {
+
+    private VerticalLayout layout = new VerticalLayout();
+    private TextField textField = new TextField("Enter name: ");
+    private Button button = new Button("Call Server");
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Label label = new Label("It works!");
-        setContent(label);
+        layout.addComponents(textField, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+
+        setContent(layout);
+        setSizeFull();
+
+        button.addClickListener(this);
+    }
+
+    @Override
+    public void buttonClick(Button.ClickEvent event) {
+        String name = textField.getValue();
+        String message = getMessage(name);
+        layout.addComponent(new Label(message));
+    }
+
+    private String getMessage(String name) {
+        String time = new SimpleDateFormat("hh:mm:ss a").format(new Date());
+        return String.format("Hello %s, the time here is %s", name, time);
     }
 
 }
